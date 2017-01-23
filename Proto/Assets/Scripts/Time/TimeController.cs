092ba0bcf1ec2f17c3e81fa5dc4f3eeb39515abb
Isaac Ouellet Therrien <isaac.computing@gmail.com>
2017-01-23 11:34:39 -0500

    Minor refactor (whitespace)

--- Proto/Assets/Scripts/Time/TimeController.cs
+++ Proto/Assets/Scripts/Time/TimeController.cs
@@ -5,26 +5,27 @@ public class TimeController : MonoBehaviour
     public delegate void Tick(int tick);
     public event Tick EventTick;
 
-	int m_Time;
-	bool isForward = true;
+    int m_Time, _maxTime;
+    bool isForward = true;
     float timer = 0;
 
-	public int time
-	{
-		get { return m_Time; }
-		set { m_Time = value; }
-	}
+    public int time
+    {
+        get { return m_Time; }
+        set { m_Time = value; }
+    }
 
-	public bool isFoward
-	{
-		get { return isForward; }
-		set { isForward = value; }
-	}
 
-	void FixedUpdate()
-	{
+    public bool isFoward
+    {
+        get { return isForward; }
+        set { isForward = value; }
+    }
+
+    void FixedUpdate()
+    {
         timer += Time.deltaTime;
-        if(timer >= 1f && isForward)
+        if (timer >= 1f && isForward)
         {
             timer = 0;
             m_Time++;
