0ecb115b6644c6ef01fb4f81aaf4a42c1d7e3022
Frédéric Carrier <frederic.carrier@usherbrooke.ca>
2017-02-08 12:55:08 -0500

    Adding doors unlock and open.

--- Proto/Assets/Scripts/Interactive/Door.cs
+++ Proto/Assets/Scripts/Interactive/Door.cs
@@ -1,6 +1,10 @@
-﻿public class Door : Interactive
+﻿using UnityEngine;
+
+public class Door : Interactive
 {
-    bool m_IsOpen = false;
+    [SerializeField]bool m_IsOpen = false;
+    [SerializeField]bool m_IsLock = true;
+    AgentActions m_Action;
 
     new void Start()
     {
@@ -9,13 +13,57 @@
         m_IsActivated = true;
     }
 
+    void OnTriggerEnter(Collider other)
+    {
+        m_Action = other.GetComponent<AgentActions>();
+        if (m_Action)
+        {
+            if (m_Action.enabled)
+            {
+                m_HUD.ShowActionPrompt("Open door.");
+                m_Action.SetInteract(true);
+                m_Action.SetInteractionObject(this);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        m_Action = other.GetComponent<AgentActions>();
+        if (m_Action)
+        {
+            if (m_Action.enabled)
+            {
+                m_HUD.HideActionPrompt();
+                m_Action.SetInteract(false);
+            }
+        }
+    }
+
     public override void Interact()
     {
-        if(!m_IsOpen)
+        if(!m_IsLock)
         {
-            m_IsOpen = true;
-            OpenDoor();
+            if(m_IsOpen)
+            {
+                m_IsOpen = false;
+                CloseDoor();
+            }
+            else
+            {
+                m_IsOpen = true;
+                OpenDoor();
+            }
         }
+        else
+        {
+            FindObjectOfType<HUD>().ShowMessages("Door is locked", 3f);
+        }
+    }
+
+    public void Unlock()
+    {
+        GetComponent<PhotonView>().RPC("RPCUnlock", PhotonTargets.All);
     }
 
     void OpenDoor()
@@ -23,12 +71,26 @@
         GetComponent<PhotonView>().RPC("RPCOpenDoor", PhotonTargets.All);
     }
 
+    void CloseDoor()
+    {
+        GetComponent<PhotonView>().RPC("RPCCloseDoor", PhotonTargets.All);
+    }
+
     [PunRPC]
     void RPCOpenDoor()
     {
-        if(PhotonNetwork.isMasterClient)
-        {
-            PhotonNetwork.Destroy(gameObject);
-        }        
+        transform.rotation = Quaternion.Euler(0,90,0);
+    }
+
+    [PunRPC]
+    void RPCCloseDoor()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+
+    [PunRPC]
+    void RPCUnlock()
+    {
+        m_IsLock = false;
     }
 }
