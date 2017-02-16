c2d3dc9a602ae894a3c2825e6b001f1ceac1d8fe
Isaac Ouellet Therrien <isaac.computing@gmail.com>
2017-02-16 14:49:37 -0500

    Clock timer (text) working and connected to the time controller (arrow too)

--- Proto/Assets/Scripts/UI/ClockUI.cs
+++ Proto/Assets/Scripts/UI/ClockUI.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ClockUI : MonoBehaviour
 {
     private Transform _seconds;
     private Transform _minutes;
     private Transform _arrow;
+    private Text _textTimeRewind;
     [SerializeField] private int _speed = 10;
+    private TimeController _timeController;
 
     private void Start()
     {
+        _timeController = FindObjectOfType<TimeController>();
+        _textTimeRewind = GetComponentInChildren<Text>();
         foreach (Transform child in transform)
         {
             if (_seconds != null && _minutes != null) break;
@@ -17,29 +22,26 @@ public class ClockUI : MonoBehaviour
             else if (child.name.IndexOf("pivotMinutes", StringComparison.InvariantCultureIgnoreCase) > -1) _minutes = child;
             else if (child.name.IndexOf("pivotArrow", StringComparison.InvariantCultureIgnoreCase) > -1) _arrow = child;
         }
+        _timeController.Tick.Suscribe(RotateLikeAClock);
+        RotateLikeAClock(0);
     }
 
-    private void Update()
+    private int RotateLikeAClock(int time)
     {
-        RotateLikeAClock(75);
+        _textTimeRewind.text = string.Format("{0}:{1:00}", time / 60, time % 60);
+        _arrow.rotation = Quaternion.Euler(0f, 0f, GetSecondClockPosition(time));
+        _seconds.rotation = Quaternion.Euler(0f, 0f, GetSecondClockPosition(time));
+        _minutes.rotation = Quaternion.Euler(0f, 0f, GetMinuteClockPosition(time));
+        return 0;
     }
 
-    private bool a = true;
-    private void RotateLikeAClock(int time)
-    {
-        if(a)_seconds.Rotate(0f, 0f, GetSecondClockPosition(time));
-        if(a)_minutes.Rotate(0f, 0f, GetMinuteClockPosition(time));
-        if(a)_arrow.Rotate(0f, 0f, GetSecondClockPosition(time));
-        a = false;
-    }
-
-    private float GetSecondClockPosition(int time)
+    private static float GetSecondClockPosition(int time)
     {
         var seconds = time % 60;
         return -(360 / 60 * seconds + 2);
     }
 
-    private float GetMinuteClockPosition(int time)
+    private static float GetMinuteClockPosition(int time)
     {
         var seconds = time / 60;
         return -(360 / 12 * seconds + 2);
