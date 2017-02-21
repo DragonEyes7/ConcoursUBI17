a939ebddc51312496f9b1fda79d7f99661530434
Isaac Ouellet Therrien <isaac.computing@gmail.com>
2017-02-21 13:07:22 -0500

    The left joystick controls the seconds, the D-Pad Y Axis controls the minutes

--- Proto/Assets/Scripts/UI/ClockUI.cs
+++ Proto/Assets/Scripts/UI/ClockUI.cs
@@ -10,11 +10,14 @@ public class ClockUI : MonoBehaviour
     private Transform _arrow;
     private Text _textTimeRewind;
     private TimeController _timeController;
-    private float _curTime;
+    private float _curTime, _prevTime;
+    private PhotonView _photonView;
+    private bool _isFirst = true;
 
     private void OnEnable()
     {
-        TimeStopper.StopTime();
+        _photonView = GetComponent<PhotonView>();
+        if(!_isFirst)_photonView.RPC("RPCStopTime", PhotonTargets.All);
         _timeController = FindObjectOfType<TimeController>();
         _textTimeRewind = GetComponentInChildren<Text>();
         foreach (Transform child in transform)
@@ -25,25 +28,38 @@ public class ClockUI : MonoBehaviour
             else if (child.name.IndexOf("pivotArrow", StringComparison.InvariantCultureIgnoreCase) > -1) _arrow = child;
         }
         Debug.Log("Time Controller time : " + _timeController.time);
-        UpdateClock(_timeController.time);
+        _curTime = _prevTime = _timeController.time;
+        UpdateClock(_curTime);
         StartCoroutine(ReadInput());
     }
 
     private void OnDisable()
     {
-        TimeStopper.StartTime();
+        if(!_isFirst)_photonView.RPC("RPCStartTime", PhotonTargets.All);
+        _isFirst = false;
     }
-
     private IEnumerator ReadInput()
     {
-        //This calculates the angle the joystick is in (instead of simple x, y coords)
-        var angle = Mathf.Atan2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * Mathf.Rad2Deg;
-        _curTime = angle / 360 * 60;
-        _curTime = _curTime < 0 ? 60 + _curTime : _curTime;
+        _curTime = TuneMinutes(Input.GetAxis("DPadY"), (int)_curTime/60) +
+                   TuneSeconds(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), (int)_curTime % 60);
+        if (_curTime > _prevTime) _curTime = _prevTime;
+        if (_curTime < 0) _curTime = 0;
         UpdateClock(_curTime);
-        //_arrow.rotation = Quaternion.Euler(new Vector3(0, 0, -angle));
         yield return new WaitForSecondsRealtime(0.01f);
         StartCoroutine(ReadInput());
+    }
+
+    private static float TuneMinutes(float y, int currentMinutes)
+    {
+        var minutes = (currentMinutes + y) * 60;
+        return minutes < 0 ? 0 : minutes;
+    }
+
+    private static float TuneSeconds(float x, float y, int prevSeconds)
+    {
+        if (x == 0 && y == 0) return prevSeconds;
+        var time = Mathf.Atan2(x, y) * Mathf.Rad2Deg / 360 * 60;
+        return time < 0 ? 60 + time : time;
     }
 
     private void UpdateClock(float time)
@@ -64,5 +80,17 @@ public class ClockUI : MonoBehaviour
     {
         var seconds = time / 60;
         return -(30 * seconds + 2);
+    }
+
+    [PunRPC]
+    private void RPCStopTime()
+    {
+        TimeStopper.StopTime();
+    }
+
+    [PunRPC]
+    private void RPCStartTime()
+    {
+        TimeStopper.StartTime();
     }
 }
