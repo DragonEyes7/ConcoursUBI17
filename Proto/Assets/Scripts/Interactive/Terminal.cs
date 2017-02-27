f6d3314fba4bdd20fca19666344452e576c485f3
Isaac Ouellet Therrien <isaac.computing@gmail.com>
2017-02-27 15:43:02 -0500

    Terminal Select/Unselect override

--- Proto/Assets/Scripts/Interactive/Terminal.cs
+++ Proto/Assets/Scripts/Interactive/Terminal.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Terminal : Interactive
 {
@@ -24,19 +25,39 @@ public class Terminal : Interactive
                 RaycastHit hit;
                     
                 Vector3 direction = m_Action.GetCenterCam().position - transform.position;
-
+                
                 if (Physics.Raycast(transform.position, direction, out hit, 25f, m_Layer))
                 {
                     Debug.DrawRay(transform.position, direction, Color.red, 5f);
                     if(hit.transform == m_Action.GetCenterCam().transform)
                     {
-                        m_HUD.ShowActionPrompt("Hack Terminal");
-                        m_Action.SetInteract(true);
-                        m_Action.SetInteractionObject(this);
                         Select();
                     }
                 }
             }
+        }
+    }
+
+    protected override void Select()
+    {
+        m_HUD.ShowActionPrompt("Hack Terminal");
+        if (m_Action)
+        {
+            m_Action.SetInteract(true);
+            m_Action.SetInteractionObject(this);
+        }
+        base.Select();
+    }
+
+    protected override void UnSelect()
+    {
+        base.UnSelect();
+        m_HUD.HideActionPrompt();
+        if (m_Action)
+        {
+            m_Action.SetInteract(false);
+            m_Action.SetInteractionObject(null);
+            m_Action = null;
         }
     }
 
@@ -50,10 +71,7 @@ public class Terminal : Interactive
                 if (m_Action.enabled)
                 {
                     UnSelect();
-                    m_HUD.HideActionPrompt();
-                    m_Action.SetInteract(false);
-                    m_Action.SetInteractionObject(null);
-                    m_Action = null;
+
                 }
             }
         }
