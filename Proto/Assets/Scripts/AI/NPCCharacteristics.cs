0c55db3875c47339e720d49e6dc3ed9748048d38
Frédéric Carrier <fredericcarrier@usherbrooke.ca>
2017-02-28 13:20:15 -0500

    Code cleaning

--- Proto/Assets/Scripts/AI/NPCCharacteristics.cs
+++ Proto/Assets/Scripts/AI/NPCCharacteristics.cs
@@ -1,22 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class NPCCharacteristics : MonoBehaviour {
+public class NPCCharacteristics : MonoBehaviour
+{
 
     public Transform HairPart, PantPart, ShirtPart;
     public Material HairMaterial, PantMaterial, ShirtMaterial;
-
-    // Use this for initialization
-    void Start () {
+    
+    void Start ()
+    {
         //Set the Material for various pieces
         HairPart.GetComponent<Renderer>().material = HairMaterial;
         PantPart.GetComponent<Renderer>().material = PantMaterial;
         ShirtPart.GetComponent<Renderer>().material = ShirtMaterial;
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
