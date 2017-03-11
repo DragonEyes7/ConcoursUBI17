20292c21f02f49dda57a2e45246fe39566098e15
Frédéric Carrier <dragon_eyes@live.fr>
2017-03-11 16:51:15 -0500

    Polish
    Typo fix and code cleaning.

--- Proto/Assets/Scripts/Level Generation/LevelGenerator.cs
+++ Proto/Assets/Scripts/Level Generation/LevelGenerator.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelGenerator : MonoBehaviour {
+public class LevelGenerator : MonoBehaviour
+{
 
     //un peu batard mais on a pas vraiment besoin de se casser la tête
     public Transform[] small_spawnLocations;
@@ -18,18 +18,18 @@ public class LevelGenerator : MonoBehaviour {
     public int nbr_of_med_shops;
     public int nbr_of_large_shops;
 
-    // Use this for initialization
-    void Start () {
+    void Start ()
+    {
        /* instanciateShops(small_spawnLocations, small_shops);
         instanciateShops(medium_spawnLocations, medium_shops);
         instanciateShops(large_spawnLocations, large_shops);*/
 
-        instanciateShops(small_spawnLocations, small_shops,nbr_of_small_shops);
-        instanciateShops(medium_spawnLocations, medium_shops,nbr_of_med_shops);
-        instanciateShops(large_spawnLocations, large_shops, nbr_of_large_shops);
+        instantiateShops(small_spawnLocations, small_shops,nbr_of_small_shops);
+        instantiateShops(medium_spawnLocations, medium_shops,nbr_of_med_shops);
+        instantiateShops(large_spawnLocations, large_shops, nbr_of_large_shops);
     }
 
-    void instanciateShops( Transform[] ts, List<GameObject> s)
+    void instantiateShops( Transform[] ts, List<GameObject> s)
     {
         int random;
 
@@ -41,20 +41,15 @@ public class LevelGenerator : MonoBehaviour {
         }
     }
 // Could help with balance 
-    void instanciateShops(Transform[] ts, List<GameObject> s, int NbrOfShop)
+    void instantiateShops(Transform[] ts, List<GameObject> s, int NbrOfShop)
     {
         int random;
 
         for(int i =0; i<NbrOfShop;i++)
         {
             random = Random.Range(0, s.Count);
-            GameObject shop = Instantiate(s[random], ts[i].transform, false);
+            Instantiate(s[random], ts[i].transform, false);
             s.RemoveAt(random);
         }
     }
-
-    // Update is called once per frame
-    void Update () {
-		
-	}
 }
