db29f736c905e53e3dcd46b79ed8707d274f91c9
Isaac Ouellet Therrien <isaac.computing@gmail.com>
2017-03-14 20:22:24 -0400

    Very simple/hacky way to update the server list on load

---
+++ Proto/Assets/Scripts/UI/OnActivateRefresh.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class OnActivateRefresh : MonoBehaviour
+{
+    private ServerUI _serverUi;
+    private void Start()
+    {
+        _serverUi = FindObjectOfType<ServerUI>();
+    }
+
+    private void OnEnable()
+    {
+        _serverUi.GetServerList();
+    }
+}
