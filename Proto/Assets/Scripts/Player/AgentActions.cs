1ed977ca955db97acfc6bb9cfdec28ba4a4baa54
Frédéric Carrier <dragon_eyes@live.fr>
2017-03-18 15:49:32 -0400

    Add animation interact.

--- Proto/Assets/Scripts/Player/AgentActions.cs
+++ Proto/Assets/Scripts/Player/AgentActions.cs
@@ -3,10 +3,14 @@
 public class AgentActions : Action
 {
     MainRecorder m_Recorder;
+    MovementPlayer _MovementPlayer;
+    PhotonView _PhotonView;
 
     new void Start()
     {
         base.Start();
+        _PhotonView = GetComponent<PhotonView>();
+        _MovementPlayer = GetComponent<MovementPlayer>();
         m_Recorder = FindObjectOfType<MainRecorder>();
     }
 
@@ -14,13 +18,27 @@ public class AgentActions : Action
     {
         if (m_Interact && Input.GetButtonDown("Action") && m_Recorder.IsRecording)
         {
-            m_Interactive.Interact();
-            m_Interact = false;
+            _MovementPlayer.CantMove();
+            transform.LookAt(new Vector3(m_Interactive.transform.position.x, transform.position.y, m_Interactive.transform.position.z));
+            _PhotonView.RPC("RPCInteract", PhotonTargets.All);
         }
         if (_intercept && Input.GetButtonDown("Uplink") && m_Recorder.IsRecording)
         {
             m_Interactive.Intercept();
             _intercept = false;
         }
+    }
+
+    [PunRPC]
+    void RPCInteract()
+    {
+        GetComponent<Animator>().SetBool("Interact", true);
+    }
+
+    public void Interact()
+    {
+        if (!m_Interactive) return;
+        m_Interactive.Interact();
+        m_Interact = false;
     }
 }
