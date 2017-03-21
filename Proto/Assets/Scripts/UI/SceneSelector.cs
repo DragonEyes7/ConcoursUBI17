7a7309b48b8f929eb5e2e321c01c1515879d041c
Isaac Ouellet Therrien <isaac.computing@gmail.com>
2017-03-21 15:28:40 -0400

    Update the level selector input at the bottom of the leaderboard

--- Proto/Assets/Scripts/UI/SceneSelector.cs
+++ Proto/Assets/Scripts/UI/SceneSelector.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class SceneSelector : MonoBehaviour
@@ -13,24 +14,43 @@ public class SceneSelector : MonoBehaviour
     private void Start()
     {
         _leaderboardUI = FindObjectOfType<LeaderboardUI>();
+        var levelShown = FindObjectOfType<LevelToLoad>().GetLevelToLoad();
+        pos = GetPosOfLevelShown(levelShown);
         _levelName.text = _levels[pos];
-        _leftArrow.gameObject.SetActive(false);
-        if (_levels.Length <= 1) _rightArrow.gameObject.SetActive(false);
+        UpdateArrows();
+    }
+
+    private int GetPosOfLevelShown(string levelShown)
+    {
+        for (var i = 0; i < _levels.Length; ++i)
+        {
+            var level = _levels[i];
+            if (level.IndexOf(levelShown, StringComparison.InvariantCultureIgnoreCase) > -1)
+                return i;
+        }
+        //Defaults at loading the first entry if nothing found
+        return 0;
     }
 
     public void LeftArrowClick()
     {
         _levelName.text = _levels[--pos];
-        if (pos == 0) _leftArrow.gameObject.SetActive(false);
-        if (_levels.Length > 1) _rightArrow.gameObject.SetActive(true);
         _leaderboardUI.LoadScene(_levels[pos]);
+        UpdateArrows();
     }
 
     public void RightArrowClick()
     {
         _levelName.text = _levels[++pos];
-        if (_levels.Length <= pos + 1)_rightArrow.gameObject.SetActive(false);
-        _leftArrow.gameObject.SetActive(true);
         _leaderboardUI.LoadScene(_levels[pos]);
+        UpdateArrows();
+    }
+
+    private void UpdateArrows()
+    {
+        _leftArrow.gameObject.SetActive(true);
+        _rightArrow.gameObject.SetActive(true);
+        if (pos == 0) _leftArrow.gameObject.SetActive(false);
+        if (_levels.Length <= pos + 1)_rightArrow.gameObject.SetActive(false);
     }
 }
