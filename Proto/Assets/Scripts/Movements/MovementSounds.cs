8a476e5b6ed2446ab8150f5422740442fdb6c0a8
Frédéric Carrier <dragon_eyes@live.fr>
2017-03-22 13:12:51 -0400

    Adding footsteps sounds

---
+++ Proto/Assets/Scripts/Movements/MovementSounds.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class MovementSounds : MonoBehaviour
+{
+    [SerializeField]AudioClip[] _FootStepsClips;
+
+    AudioSource _AudioSource;
+
+    protected void Start ()
+	{
+        _AudioSource = GetComponent<AudioSource>();
+    }
+
+    void PlayFootSteps()
+    {
+        _AudioSource.clip = _FootStepsClips[Random.Range(0, _FootStepsClips.Length)];
+        _AudioSource.pitch = Random.Range(0.80f, 1.2f);
+        _AudioSource.Play();
+    }
+}
